f68c5f1d8b32e1d40c9fac3ca598240abe00bdeb
Mateus Villa Nova <villa1999nova@gmail.com>
2024-07-11 13:25:33 -0300

    feat/chore/MVN-019: New grid library and finished grid

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,25 @@ const config: Config = {
       screens: {
         nano: '336px',
       },
+      gridTemplateAreas: {
+        'nano-layout': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+        'sm-layout': ['A B', 'E D', 'F D', 'C C', 'G H', 'G H'],
+        'md-layout': ['A B B', 'C C D', 'E F D', 'G G H', 'G G H'],
+        'lg-layout': [],
+      },
+      gridTemplateColumns: {
+        'nano-layout': 'repeat(1, minmax(0, 36.25rem))',
+        'sm-layout': 'repeat(2, minmax(0, 30.75rem))',
+        'md-layout': 'repeat(3, minmax(0, 26.875rem))',
+        'lg-layout': '',
+      },
+      gridTemplateRows: {
+        'nano-layout': '',
+        'sm-layout': '',
+        'md-layout': '',
+        'lg-layout': '',
+        'xl-layout': 'repeat(4, minmax(203px, auto))',
+      },
     },
   },
   plugins: [require('@savvywombat/tailwindcss-grid-areas')],
