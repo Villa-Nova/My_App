cb15bba18865b77b2961789c4fce19877fb89329
Mateus Villa Nova <villa1999nova@gmail.com>
2024-08-19 15:34:26 -0300

    fix/MVN-035: Discord Status Redesigned

--- src/app/components/GridLayout/index.tsx
+++ src/app/components/GridLayout/index.tsx
@@ -1,10 +1,11 @@
+import { DiscordStatus } from '@/components/DiscordStatus'
 import { Switcher } from '@/components/Switcher'
 
 export function GridLayout() {
   return (
     <aside className="flex-[2] px-8 py-7 max-h-dvh xl:overflow-y-scroll">
       <div className="grid gap-4 rounded_border grid-areas-nano-layout grid-cols-nano-layout grid-rows-nano-layout sm:grid-cols-sm-layout sm:grid-areas-sm-layout md:grid-areas-md-layout md:grid-cols-md-layout xl:grid-rows-xl-layout">
-        <div className="bg-indigo-100 dark:bg-rose-400 aspect-square w-full h-full grid-in-A flex justify-center items-center">
+        <div className="bg-indigo-100 aspect-square w-full h-full grid-in-A flex justify-center items-center">
           <Switcher />
         </div>
         <div className="bg-indigo-200 aspect-[16/5] w-full h-full grid-in-B">
@@ -16,8 +17,8 @@ export function GridLayout() {
         <div className="bg-indigo-400 aspect-[2/3] w-full h-full grid-in-D">
           4
         </div>
-        <div className="bg-indigo-500 aspect-square w-full h-full grid-in-E">
-          Discord
+        <div className="bg-indigo-500 aspect-square w-full h-full grid-in-E relative">
+          <DiscordStatus />
         </div>
         <div className="bg-indigo-600 aspect-square w-full h-full grid-in-F">
           Github
