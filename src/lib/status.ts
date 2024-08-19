144b077d7cdb62c9a91e63e40ae5d0c13ff9da8b
Mateus Villa Nova <villa1999nova@gmail.com>
2024-08-19 13:59:22 -0300

    feat/MVN-033: .env

--- src/lib/status.ts
+++ src/lib/status.ts
@@ -1,10 +1,10 @@
 export type Status = 'idle' | 'dnd' | 'online' | 'offline'
 
 export const handleStatus = async () => {
-  const userId = '364447483381678080'
+  const userId = process.env.NEXT_PUBLIC_DISCORD_USER_ID
 
   const response: Status = await fetch(
-    `https://api.lanyard.rest/v1/users/${userId}`,
+    `${process.env.NEXT_PUBLIC_DISCORD_URL}/${userId}`,
   )
     .then((res) => res.json())
     .then((res) => {
