4ddce61c77724a15bfbd55d63a050a6b4513e4bc
Mateus Villa Nova <villa1999nova@gmail.com>
2024-08-21 11:33:49 -0300

    fix/MVN-039: url breakpoint added to a constant

--- src/lib/status.ts
+++ src/lib/status.ts
@@ -2,10 +2,9 @@ export type Status = 'idle' | 'dnd' | 'online' | 'offline'
 
 export const handleStatus = async () => {
   const userId = process.env.NEXT_PUBLIC_DISCORD_USER_ID
+  const url = process.env.NEXT_PUBLIC_DISCORD_URL
 
-  const response: Status = await fetch(
-    `${process.env.NEXT_PUBLIC_DISCORD_URL}/${userId}`,
-  )
+  const response: Status = await fetch(`${url}/${userId}`)
     .then((res) => res.json())
     .then((res) => {
       return res?.data?.discord_status
